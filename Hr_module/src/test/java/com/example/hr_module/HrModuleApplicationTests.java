bd0b29ebaf572cf29a5a254d73d8930e14f1352a
radek <radek.salay@gmail.com>
2023-08-17 19:30:56 +0200

    Mock dataSource for test. Create application.yaml fort tests. Add TODO for password as plaintext.

--- Hr_module/src/test/java/com/example/hr_module/HrModuleApplicationTests.java
+++ Hr_module/src/test/java/com/example/hr_module/HrModuleApplicationTests.java
@@ -1,10 +1,23 @@
 package com.example.hr_module;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
+import javax.sql.DataSource;
+
+@SpringJUnitConfig
 @SpringBootTest
+@AutoConfigureTestDatabase
+@TestPropertySource("/application.yaml")
 class HrModuleApplicationTests {
+
+    @MockBean
+    private DataSource dataSourceMock;
 
     @Test
     void contextLoads() {
--- TODO.md
+++ TODO.md
@@ -2,3 +2,6 @@
 ## PDF
 Rest Api for uploading PDF files. From the files, system reads who is customer and what delivery is needed,
 if the customer does not exist, he will be created with delivery.
+
+## Security
+Solve issue with DB password as plain text in application.yaml.
